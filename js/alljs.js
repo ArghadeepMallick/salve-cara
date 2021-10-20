34230221ed557cbe79f007232e407203bf3d1bec
ArghadeepMallick <arghadeep@mediastic.in>
2021-10-20 18:28:01 +0530

    single product page

---
+++ js/alljs.js
@@ -0,0 +1,350 @@
+$(document).ready(function () {
+
+    $("#certificate-icon").owlCarousel({
+        loop: true,
+        nav: true,
+        margin: 0,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 5,
+            },
+            600: {
+                items: 6,
+            },
+            1000: {
+                items: 7,
+            },
+        },
+    });
+    $("#top_trending").owlCarousel({
+        loop: true,
+        nav: true,
+        margin: 20,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            600: {
+                items: 2,
+            },
+            1000: {
+                items: 4,
+            },
+        },
+    });
+    $("#top_trending1").owlCarousel({
+        loop: true,
+        nav: false,
+        margin: 20,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            600: {
+                items: 2,
+            },
+            1000: {
+                items: 4,
+            },
+        },
+    });
+    $("#top_trending2").owlCarousel({
+        loop: true,
+        nav: false,
+        margin: 20,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            600: {
+                items: 2,
+            },
+            1000: {
+                items: 4,
+            },
+        },
+    });
+    $("#top_trending_single").owlCarousel({
+        loop: true,
+        nav: false,
+        margin: 20,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            600: {
+                items: 2,
+            },
+            1000: {
+                items: 4,
+            },
+        },
+    });
+    $("#top_mamaearth").owlCarousel({
+        loop: true,
+        nav: false,
+        margin: 20,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            600: {
+                items: 2,
+            },
+            1000: {
+                items: 4,
+            },
+        },
+    });
+    $("#video").owlCarousel({
+        loop: true,
+        nav: true,
+        margin: 20,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 2,
+            },
+            600: {
+                items: 2,
+            },
+            1000: {
+                items: 5,
+            },
+        },
+    });
+    $("#video1").owlCarousel({
+        loop: true,
+        nav: true,
+        margin: 20,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 2,
+            },
+            600: {
+                items: 2,
+            },
+            1000: {
+                items: 5,
+            },
+        },
+    });
+    $("#video2").owlCarousel({
+        loop: true,
+        nav: true,
+        margin: 20,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 2,
+            },
+            600: {
+                items: 2,
+            },
+            1000: {
+                items: 5,
+            },
+        },
+    });
+    $("#key_ing").owlCarousel({
+        loop: true,
+        nav: true,
+        margin: 100,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 2,
+            },
+            600: {
+                items: 2,
+            },
+            1000: {
+                items: 5,
+            },
+        },
+    });
+    $("#brands").owlCarousel({
+        loop: true,
+        nav: true,
+        margin: 20,
+        responsiveClass: true,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        autoplayHoverPause: false,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            600: {
+                items: 2,
+            },
+            1000: {
+                items: 4,
+            },
+        },
+    });
+    $("#big_offer").owlCarousel({
+        loop: true,
+        nav: true,
+        margin: 20,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            600: {
+                items: 2,
+            },
+            1000: {
+                items: 3,
+            },
+        },
+    });
+    $("#big_offer1").owlCarousel({
+        loop: true,
+        nav: true,
+        margin: 20,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            600: {
+                items: 2,
+            },
+            1000: {
+                items: 3,
+            },
+        },
+    });
+    $("#big_offer2").owlCarousel({
+        loop: true,
+        nav: true,
+        margin: 20,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            600: {
+                items: 2,
+            },
+            1000: {
+                items: 3,
+            },
+        },
+    });
+    $("#hair_oil").owlCarousel({
+        loop: true,
+        nav: true,
+        margin: 100,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            600: {
+                items: 2,
+            },
+            1000: {
+                items: 3,
+            },
+        },
+    });
+    $("#hair_oil1").owlCarousel({
+        loop: true,
+        nav: true,
+        margin: 100,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            600: {
+                items: 2,
+            },
+            1000: {
+                items: 3,
+            },
+        },
+    });
+    $("#up_to").owlCarousel({
+        loop: true,
+        nav: false,
+        margin: 20,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            600: {
+                items: 2,
+            },
+            1000: {
+                items: 4,
+            },
+        },
+    });
+    $("#shop").owlCarousel({
+        loop: true,
+        nav: false,
+        margin: 50,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            600: {
+                items: 2,
+            },
+            1000: {
+                items: 4,
+            },
+        },
+    });
+    $("#shop1").owlCarousel({
+        loop: true,
+        nav: false,
+        margin: 50,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            600: {
+                items: 2,
+            },
+            1000: {
+                items: 4,
+            },
+        },
+    });
+    $("#single-product").owlCarousel({
+        dotsContainer: '#carousel-custom-dots',
+        responsive: {
+            0: {
+                items: 1.2,
+                nav: false,
+                loop: false,
+            },
+            600: {
+                items: 1.2,
+                nav: false,
+                loop: false,
+            },
+            1000: {
+                items: 1,
+                nav: true,
+                loop: false,
+            }
+        }
+    });
+
+});
