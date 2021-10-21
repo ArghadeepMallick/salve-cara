052082d750ccc2ffdc31b9fd07818853b766aaf5
ArghadeepMallick <arghadeep@mediastic.in>
2021-10-22 00:19:55 +0530

    updated Mobile UI

--- js/alljs.js
+++ js/alljs.js
@@ -1,16 +1,36 @@
 $(document).ready(function () {
-
+    $("#top-bar").owlCarousel({
+        loop: true,
+        nav: false,
+        dots: false,
+        margin: 0,
+        responsiveClass: true,
+        autoplay:true,
+        autoplayTimeout:3000,
+        autoplayHoverPause:false,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            600: {
+                items: 1,
+            },
+            1000: {
+                items: 1,
+            },
+        },
+    }); 
     $("#certificate-icon").owlCarousel({
         loop: true,
         nav: true,
-        margin: 0,
+        margin: 10,
         responsiveClass: true,
         responsive: {
             0: {
-                items: 5,
+                items: 4,
             },
             600: {
-                items: 6,
+                items: 5,
             },
             1000: {
                 items: 7,
@@ -109,10 +129,10 @@ $(document).ready(function () {
         responsiveClass: true,
         responsive: {
             0: {
-                items: 2,
+                items: 1,
             },
             600: {
-                items: 2,
+                items: 3,
             },
             1000: {
                 items: 5,
@@ -126,10 +146,10 @@ $(document).ready(function () {
         responsiveClass: true,
         responsive: {
             0: {
-                items: 2,
+                items: 1,
             },
             600: {
-                items: 2,
+                items: 3,
             },
             1000: {
                 items: 5,
@@ -143,10 +163,10 @@ $(document).ready(function () {
         responsiveClass: true,
         responsive: {
             0: {
-                items: 2,
+                items: 1,
             },
             600: {
-                items: 2,
+                items: 3,
             },
             1000: {
                 items: 5,
@@ -156,17 +176,20 @@ $(document).ready(function () {
     $("#key_ing").owlCarousel({
         loop: true,
         nav: true,
-        margin: 100,
+        
         responsiveClass: true,
         responsive: {
             0: {
                 items: 2,
+                margin: 10,
             },
             600: {
                 items: 2,
+                margin: 50,
             },
             1000: {
                 items: 5,
+                margin: 100,
             },
         },
     });
@@ -243,35 +266,42 @@ $(document).ready(function () {
     });
     $("#hair_oil").owlCarousel({
         loop: true,
-        nav: true,
-        margin: 100,
+        nav: false,
+        dots: false,
+        
         responsiveClass: true,
         responsive: {
             0: {
                 items: 1,
+                margin: 10,
             },
             600: {
                 items: 2,
+                margin: 50,
             },
             1000: {
                 items: 3,
+                margin: 100,
             },
         },
     });
     $("#hair_oil1").owlCarousel({
         loop: true,
-        nav: true,
-        margin: 100,
+        nav: false,
+        dots: false,
         responsiveClass: true,
         responsive: {
             0: {
                 items: 1,
+                margin: 10,
             },
             600: {
                 items: 2,
+                margin: 50,
             },
             1000: {
                 items: 3,
+                margin: 100,
             },
         },
     });
@@ -302,7 +332,7 @@ $(document).ready(function () {
                 items: 1,
             },
             600: {
-                items: 2,
+                items: 3,
             },
             1000: {
                 items: 4,
@@ -319,7 +349,7 @@ $(document).ready(function () {
                 items: 1,
             },
             600: {
-                items: 2,
+                items: 3,
             },
             1000: {
                 items: 4,
@@ -330,12 +360,12 @@ $(document).ready(function () {
         dotsContainer: '#carousel-custom-dots',
         responsive: {
             0: {
-                items: 1.2,
+                items: 1,
                 nav: false,
                 loop: false,
             },
             600: {
-                items: 1.2,
+                items: 1.3,
                 nav: false,
                 loop: false,
             },
@@ -345,6 +375,53 @@ $(document).ready(function () {
                 loop: false,
             }
         }
+    });
+
+});
+
+function darken_screen(yesno) {
+    if (yesno == true) {
+        document.querySelector('.screen-darken').classList.add('active');
+    }
+    else if (yesno == false) {
+        document.querySelector('.screen-darken').classList.remove('active');
+    }
+}
+
+function close_offcanvas() {
+    darken_screen(false);
+    document.querySelector('.mobile-offcanvas.show').classList.remove('show');
+    document.body.classList.remove('offcanvas-active');
+}
+
+function show_offcanvas(offcanvas_id) {
+    darken_screen(true);
+    document.getElementById(offcanvas_id).classList.add('show');
+    document.body.classList.add('offcanvas-active');
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll('[data-trigger]').forEach(function (everyelement) {
+
+        let offcanvas_id = everyelement.getAttribute('data-trigger');
+
+        everyelement.addEventListener('click', function (e) {
+            e.preventDefault();
+            show_offcanvas(offcanvas_id);
+
+        });
+    });
+
+    document.querySelectorAll('.btn-close').forEach(function (everybutton) {
+
+        everybutton.addEventListener('click', function (e) {
+            e.preventDefault();
+            close_offcanvas();
+        });
+    });
+
+    document.querySelector('.screen-darken').addEventListener('click', function (event) {
+        close_offcanvas();
     });
 
 });
